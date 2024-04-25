84f0cf259a38ae4b6bdb96560de5c460125d0dba
RunjingQ <84996752+RunjingQ@users.noreply.github.com>
2024-04-25 14:16:36 -0400

    Merged the log in page and the fragments

--- app/src/main/java/edu/uga/cs/rideshare/LogInPage.java
+++ app/src/main/java/edu/uga/cs/rideshare/LogInPage.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInPage extends AppCompatActivity {
+
+    private Button button;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,24 @@ public class LogInPage extends AppCompatActivity {
         int mode = intent.getIntExtra("log in / sign up", 1);
 
         Log.d("LogInPage", "Mode: " + mode);
+
+
+        button = findViewById( R.id.button2 );
+        email = findViewById( R.id.userPassword);
+        password = findViewById( R.id.userPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // create the intent to start the child activity
+                Intent intent = new Intent( v.getContext(), AppActivity.class );
+                intent.putExtra("email", email.getText().toString());
+                v.getContext().startActivity( intent );
+
+            }
+        });
 
 
     }
