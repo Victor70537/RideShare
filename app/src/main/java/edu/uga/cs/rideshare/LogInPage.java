3d371142991fdfab0978c05feb8a5fdf9fef5393
RunjingQ <84996752+RunjingQ@users.noreply.github.com>
2024-04-28 16:11:48 -0400

    Added stuff to differentiate between the different types of users as well as how to retrieve the current user through firebase

--- app/src/main/java/edu/uga/cs/rideshare/LogInPage.java
+++ app/src/main/java/edu/uga/cs/rideshare/LogInPage.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,9 +22,17 @@ import com.google.firebase.auth.FirebaseUser;
 public class LogInPage extends AppCompatActivity {
 
     private Button button;
+
     private EditText email;
     private EditText password;
-    private FirebaseUser user;
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1, radioButton2;
+
+//    private String userID;
+
+    private int userStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,68 +50,112 @@ public class LogInPage extends AppCompatActivity {
         email = findViewById( R.id.userPassword);
         password = findViewById( R.id.userPassword);
 
+        radioGroup = findViewById( R.id.RadioGroup );
+        radioButton1 = findViewById( R.id.radioButton );
+        radioButton2 = findViewById( R.id.radioButton2 );
+
         if (mode == 1) {
             button.setText("Log In");
         } else if (mode == 2) {
             button.setText("Sign Up");
         }
 
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                RadioButton selectedRadioButton = (RadioButton) findViewById(checkedId);
+//                String selectedValue = selectedRadioButton.getText().toString();
+//                // Handle the selected value
+//
+//                if (selectedValue.equals("Rider")) {
+//                    userStatus = 1; // user is logged in as a rider
+//                } else if (selectedValue.equals("Driver")){
+//                    userStatus = 2; // user is logged in as a driver
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Select to log in as rider or driver",
+//                            Toast.LENGTH_SHORT).show();
+//
+//                }
+//            }
+//        });
+
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                if (mode == 1) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
 
-                    FirebaseAuth mAuth = FirebaseAuth.getInstance();
-                    mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                            .addOnCompleteListener(LogInPage.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        user = mAuth.getCurrentUser();
-                                        Log.d("Log In page", "signInWithEmail: success");
+                if (selectedId != -1) {
+                    RadioButton selectedRadioButton = (RadioButton) findViewById(selectedId);
+                    String selectedValue = selectedRadioButton.getText().toString();
+                    // Handle the selected value
+                    if (selectedValue.equals("Rider")) {
+                        userStatus = 1; // user is logged in as a rider
+                    } else if (selectedValue.equals("Driver")){
+                        userStatus = 2; // user is logged in as a driver
+                    }
+                } else {
+                    // No radio button is selected
+                    Toast.makeText(getApplicationContext(), "Select to log in as rider or driver",
+                            Toast.LENGTH_SHORT).show();
+                }
 
-                                        // create the intent to start the child activity
-                                        Intent intent = new Intent( v.getContext(), RideShareMain.class );
-                                        intent.putExtra("user", user);
-                                        v.getContext().startActivity( intent );
+                if (selectedId != -1) {
 
+                    if (mode == 1) {
+
+                        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                                .addOnCompleteListener(LogInPage.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+//                                            userID = mAuth.getCurrentUser().getUid();
+                                            Log.d("Log In page", "signInWithEmail: success");
+
+                                            // create the intent to start the child activity
+                                            Intent intent = new Intent(v.getContext(), RideShareMain.class);
+//                                            intent.putExtra("user", userID);
+                                            intent.putExtra("user mode", userStatus);
+                                            v.getContext().startActivity(intent);
+
+                                        } else {
+                                            // Sign up failed, handle failure scenario (e.g., display error message)
+                                            Log.d("Log In Page", "signInWithEmail:failure", task.getException());
+                                        }
                                     }
-                                    else {
-                                        // Sign up failed, handle failure scenario (e.g., display error message)
-                                        Log.d("Log In Page", "signInWithEmail:failure", task.getException() );
+                                });
+
+
+                    } else if (mode == 2) {
+
+                        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                                .addOnCompleteListener(LogInPage.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign up successful, handle success scenario (e.g., navigate to main app)
+//                                            userID = mAuth.getCurrentUser().getUid();
+                                            Log.d("Log In Page", "createUserWithEmailAndPassword:success");
+
+                                            // create the intent to start the child activity
+                                            Intent intent = new Intent(v.getContext(), RideShareMain.class);
+//                                            intent.putExtra("user", userID);
+                                            intent.putExtra("user mode", userStatus);
+                                            v.getContext().startActivity(intent);
+
+                                            // You can optionally save additional user data to the Firebase Realtime Database here, but user creation happens through Firebase Authentication
+                                        } else {
+                                            // Sign up failed, handle failure scenario (e.g., display error message)
+                                            Log.d("Log In Page", "createUserWithEmailAndPassword:failure", task.getException());
+                                        }
                                     }
-                                }
-                            });
+                                });
 
-
-                } else if (mode == 2) {
-
-                    FirebaseAuth mAuth = FirebaseAuth.getInstance();
-                    mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                            .addOnCompleteListener(LogInPage.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign up successful, handle success scenario (e.g., navigate to main app)
-                                        user = mAuth.getCurrentUser();
-                                        Log.d("Log In Page", "createUserWithEmailAndPassword:success");
-
-                                        // create the intent to start the child activity
-                                        Intent intent = new Intent( v.getContext(), RideShareMain.class );
-                                        intent.putExtra("user", user);
-                                        v.getContext().startActivity( intent );
-
-                                        // You can optionally save additional user data to the Firebase Realtime Database here, but user creation happens through Firebase Authentication
-                                    } else {
-                                        // Sign up failed, handle failure scenario (e.g., display error message)
-                                        Log.d("Log In Page", "createUserWithEmailAndPassword:failure", task.getException() );
-                                    }
-                                }
-                            });
-
+                    }
                 }
 
 
--- app/src/main/java/edu/uga/cs/rideshare/RideShareMain.java
+++ app/src/main/java/edu/uga/cs/rideshare/RideShareMain.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RideShareMain extends AppCompatActivity {
 
@@ -18,18 +22,43 @@ public class RideShareMain extends AppCompatActivity {
     private Button ProfileButton;
     private Button LogoutButton;
 
+    private int userStatus;
+    private FirebaseUser currentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride_share_main);
+
+        Title = findViewById( R.id.Title );
+        Username = findViewById( R.id.Username );
 
         RequestButton = findViewById( R.id.RequestButton );
         ListButton = findViewById( R.id.ListButton );
         ProfileButton = findViewById( R.id.ProfileButton );
         LogoutButton = findViewById( R.id.LogoutButton );
 
+        // get the extra and set the UI appropriately
+        Intent intent = getIntent();
+        userStatus = intent.getIntExtra("user mode", 1);
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        Log.d("RideShareMain", "This is the current user:" + currentUser.getEmail());
+
+        // set the texts:
+        if (userStatus == 1) {
+            Title.setText("Use the following buttons to request a ride and view your own ride requests");
+            Username.setText(currentUser.getEmail());
+
+        } else if (userStatus == 2) {
+            Title.setText("Use the following buttons to accept a ride and view your own ride offers");
+            Username.setText(currentUser.getEmail());
+        }
+
+
         RequestButton.setOnClickListener( new RequestButtonClickListener() );
         ListButton.setOnClickListener (new ListButtonClickListener() );
+
 
 
 
