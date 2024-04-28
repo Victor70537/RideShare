d26a9accc26c6c195b2922149755cccef74af6fb
RunjingQ <84996752+RunjingQ@users.noreply.github.com>
2024-04-28 17:02:32 -0400

    Working RidesListActivity and NewRideActivity

--- app/src/main/java/edu/uga/cs/rideshare/RidesListActivity.java
+++ app/src/main/java/edu/uga/cs/rideshare/RidesListActivity.java
@@ -1,15 +1,27 @@
 package edu.uga.cs.rideshare;
 
+import static java.security.AccessController.getContext;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RidesListActivity extends AppCompatActivity {
+
+    public static final String DEBUG_TAG = "RidesListActivity";
 
     private List<Ride> ridesList;
 
@@ -21,6 +33,47 @@ public class RidesListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rides_list);
 
         ridesList = new ArrayList<Ride>();
+
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("rides");
+
+        // Set up a listener (event handler) to receive a value for the database reference.
+        // This type of listener is called by Firebase once by immediately executing its onDataChange method
+        // and then each time the value at Firebase changes.
+        //
+        // This listener will be invoked asynchronously, hence no need for an AsyncTask class, as in the previous apps
+        // to maintain job leads.
+        myRef.addValueEventListener( new ValueEventListener() {
+
+            @Override
+            public void onDataChange( @NonNull DataSnapshot snapshot ) {
+                // Once we have a DataSnapshot object, we need to iterate over the elements and place them on our job lead list.
+                ridesList.clear(); // clear the current content; this is inefficient!
+                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                    Ride ride = postSnapshot.getValue(Ride.class);
+                    ride.setKey( postSnapshot.getKey() );
+                    ridesList.add( ride );
+                    Log.d( DEBUG_TAG, "ValueEventListener: added: " + ride );
+                    Log.d( DEBUG_TAG, "ValueEventListener: key: " + postSnapshot.getKey() );
+                }
+
+                Log.d( DEBUG_TAG, "ValueEventListener: notifying recyclerAdapter" );
+
+                LinearLayout parentLayout = findViewById( R.id.parentLayout );
+
+                for (int i = 0; i < ridesList.size(); i++) {
+                    TextView textView = new TextView(getApplicationContext());
+                    textView.setText(ridesList.get(i).toString());
+                    parentLayout.addView(textView);
+                }
+
+            }
+
+            @Override
+            public void onCancelled( @NonNull DatabaseError databaseError ) {
+                System.out.println( "ValueEventListener: reading failed: " + databaseError.getMessage() );
+            }
+        } );
 
     }
 }
