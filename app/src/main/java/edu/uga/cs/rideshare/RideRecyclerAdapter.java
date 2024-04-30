c259119a335eeef6e672423a5af4d8e7dac53158
natalygary <94802299+natalygary@users.noreply.github.com>
2024-04-30 19:10:16 -0400

    separated rides

---
+++ app/src/main/java/edu/uga/cs/rideshare/RideRecyclerAdapter.java
@@ -0,0 +1,88 @@
+package edu.uga.cs.rideshare;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RideRecyclerAdapter extends RecyclerView.Adapter<RideRecyclerAdapter.RideHolder> {
+
+    public static final String DEBUG_TAG = "RideRecyclerAdapter";
+
+    private List<Ride> rideList;
+    private Context context;
+
+    public RideRecyclerAdapter(List<Ride> rideList, Context context) {
+        this.rideList = rideList;
+        this.context = context;
+    }
+
+    class RideHolder extends RecyclerView.ViewHolder {
+
+        TextView rider;
+        TextView driver;
+        TextView phone;
+        TextView destination;
+        TextView comments;
+
+        public RideHolder(View itemView) {
+            super(itemView);
+
+            rider = itemView.findViewById( R.id.rider );
+            driver = itemView.findViewById( R.id.driver );
+            phone = itemView.findViewById( R.id.phone );
+            destination = itemView.findViewById( R.id.destination );
+            comments = itemView.findViewById( R.id.comments );
+        }
+    }
+
+    @NonNull
+    @Override
+    public RideHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ride, parent, false);
+        return new RideHolder( view );
+    }
+
+    @Override
+    public void onBindViewHolder(RideHolder holder, int position) {
+        Ride ride = rideList.get(position);
+
+        Log.d( DEBUG_TAG, "onBindViewHolder: " + ride );
+
+        String key = ride.getKey();
+        String rider = ride.getRider();
+        String driver = ride.getDriver();
+        String phone = ride.getPhone();
+        String destination = ride.getDestination();
+        String comments = ride.getComments();
+
+        holder.rider.setText( ride.getRider());
+        holder.driver.setText(ride.getDriver());
+        holder.phone.setText( ride.getPhone() );
+        holder.destination.setText( ride.getDestination() );
+        holder.comments.setText( ride.getComments() );
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                EditRideDialogFragment editRideFragment =
+//                        EditRideDialogFragment.newInstance( holder.getAdapterPosition(), key, rider, driver, phone, destination, comments );
+//                editRideFragment.show( ((AppCompatActivity)context).getSupportFragmentManager(), null);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return rideList.size();
+    }
+}
+
--- app/src/main/java/edu/uga/cs/rideshare/RidesListActivity.java
+++ app/src/main/java/edu/uga/cs/rideshare/RidesListActivity.java
@@ -4,12 +4,17 @@ import static java.security.AccessController.getContext;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,6 +28,8 @@ public class RidesListActivity extends AppCompatActivity {
 
     public static final String DEBUG_TAG = "RidesListActivity";
 
+    private RecyclerView recyclerView;
+    private RideRecyclerAdapter recyclerAdapter;
     private List<Ride> ridesList;
 
     private FirebaseDatabase database;
@@ -32,7 +39,24 @@ public class RidesListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rides_list);
 
+        recyclerView = findViewById( R.id.recyclerView );
+
+        FloatingActionButton floatingButton = findViewById(R.id.floatingActionButton);
+        floatingButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                DialogFragment newFragment = new AddJobLeadDialogFragment();
+//                newFragment.show( getSupportFragmentManager(), null);
+            }
+        });
+
         ridesList = new ArrayList<Ride>();
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerAdapter = new RideRecyclerAdapter( ridesList, RidesListActivity.this );
+        recyclerView.setAdapter( recyclerAdapter );
 
         database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("rides");
@@ -59,13 +83,14 @@ public class RidesListActivity extends AppCompatActivity {
 
                 Log.d( DEBUG_TAG, "ValueEventListener: notifying recyclerAdapter" );
 
-                LinearLayout parentLayout = findViewById( R.id.parentLayout );
-
-                for (int i = 0; i < ridesList.size(); i++) {
-                    TextView textView = new TextView(getApplicationContext());
-                    textView.setText(ridesList.get(i).toString());
-                    parentLayout.addView(textView);
-                }
+//                LinearLayout parentLayout = findViewById( R.id.parentLayout );
+//
+//                for (int i = 0; i < ridesList.size(); i++) {
+//                    TextView textView = new TextView(getApplicationContext());
+//                    textView.setText(ridesList.get(i).toString());
+//                    parentLayout.addView(textView);
+//                }
+                recyclerAdapter.notifyDataSetChanged();
 
             }
 
